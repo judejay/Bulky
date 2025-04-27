5785bb789478055ac79f832f19014b8dc082f8b1
judejay <judejay26@gmail.com>
2025-04-27 16:56:14 +0100

    Add project files.

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                List<Category> objCategoryList = await _db.Categories.ToListAsync();
+                return View(objCategoryList);
+            }
+            catch (Exception ex)
+            {
+                
+                return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category name.")]
+        [Display(Name = "Category Name")]
+        public required string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
