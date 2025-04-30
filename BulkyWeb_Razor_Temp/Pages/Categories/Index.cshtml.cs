0f1625fa0b1b8a2ec1e3fba4591bf67401cc76f2
judejay <judejay26@gmail.com>
2025-04-30 13:17:18 +0100

    Refactor navigation links and model constructor
    - Removed `asp-route` attributes from Edit and Delete buttons in `Index.cshtml` for simplicity.
    - Updated `IndexModel` constructor in `Index.cshtml.cs` to directly use `ApplicationDbContext` parameter and modified `CategoresList` to access categories through this parameter.
    - Eliminated `asp-area` attributes from navigation links in `_Layout.cshtml` to streamline page routing.

--- BulkyWeb_Razor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWeb_Razor_Temp/Pages/Categories/Index.cshtml.cs
@@ -5,19 +5,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWeb_Razor_Temp.Pages.Categories
 {
-    public class IndexModel : PageModel
+    public class IndexModel(ApplicationDbContext db) : PageModel
     {
-        private readonly ApplicationDbContext _db;
+        public List<Category>? CategoresList { get; set; }
 
-        public List<Category> CategoresList { get; set; }
-
-        public IndexModel(ApplicationDbContext db)
-        {
-            _db = db;   
-        }
         public void OnGet()
         {
-            CategoresList = [.. _db.Categories];
+            CategoresList = [.. db.Categories];
         }
     }
 }
